038f2814b725bbb67c6089936a149bd12edc9db8
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-02-02 23:56:52 -0800

    tailwind config done

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    // Test out the cool functionality of tailwind - THIS IS NOT AN ACTUAL BLOCK OF CODE IN OUR APP
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        <img className="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">ChitChat</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
     </div>
   );
 };
