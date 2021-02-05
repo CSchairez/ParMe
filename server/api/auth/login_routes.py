5cb03e22f6012a797ec5db855f754c2a6da060ac
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-02-04 21:45:13 -0800

    comments added for pseudo code

--- server/api/auth/login_routes.py
+++ server/api/auth/login_routes.py
@@ -11,20 +11,22 @@ def login():
     # pull out the username and the email
     user_email = request.json.get("email", None)
     user_pass = request.json.get("password", None)
-
-    # hash the entered password with bcrypt
-    # pass_hash = bcrypt.hashpw(user_pass.encode('utf-8'), bcrypt.gensalt())
     
-    # create a new dictionary of the username and the hashed password
-    user_data = {
-        "email": user_email,
-        "password": pass_hash,
-    }
+    '''
+    Lookup the user in the database using a query ----
+        If the user exists, then we need to compare the entered password to the hashed password in the database
+            If the PW matches, we want to get the users ID and encode that in a JWT and send the jwt to the client
+            to be put in local storage (send it back as a json response like {user_id: the_jwt}). The JWT will be our form of
+            auth state being carried about across requests and qerying the DB by that user ID
+            Else, return a status code of 404 and a message saying the password was incorrect
+        Else, return a bad status and a message saying email does not exist
+    '''
 
+    # remove this return after the above algorithm is done
     return jsonify(user_data)
 
 @auth.route('/register', methods=['POST'])    # prefixed with /api/auth
-def login():
+def register():
     
     # Get the data from the body of the request
     data = request.get_json()
@@ -33,6 +35,8 @@ def login():
     user_email = request.json.get("email", None)
     user_pass = request.json.get("password", None)
     user_name = request.json.get("name", None)
+
+    # Do some validation later (joi for NodeJS. See equivalent for flask)
 
     # hash the entered password with bcrypt
     pass_hash = bcrypt.hashpw(user_pass.encode('utf-8'), bcrypt.gensalt())
