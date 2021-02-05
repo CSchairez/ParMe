ccb040e96bae6c4ed7621baba4d0f764a21ce8d0
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-02-04 21:31:20 -0800

    register

--- server/api/auth/login_routes.py
+++ server/api/auth/login_routes.py
@@ -13,7 +13,32 @@ def login():
     user_pass = request.json.get("password", None)
 
     # hash the entered password with bcrypt
+    # pass_hash = bcrypt.hashpw(user_pass.encode('utf-8'), bcrypt.gensalt())
+    
+    # create a new dictionary of the username and the hashed password
+    user_data = {
+        "email": user_email,
+        "password": pass_hash,
+    }
+
+    return jsonify(user_data)
+
+@auth.route('/register', methods=['POST'])    # prefixed with /api/auth
+def login():
+    
+    # Get the data from the body of the request
+    data = request.get_json()
+
+    # pull out the username and the email
+    user_email = request.json.get("email", None)
+    user_pass = request.json.get("password", None)
+    user_name = request.json.get("name", None)
+
+    # hash the entered password with bcrypt
     pass_hash = bcrypt.hashpw(user_pass.encode('utf-8'), bcrypt.gensalt())
+
+    # Use the user model to create a new instance of a user and then put them in the DB
+
     
     # create a new dictionary of the username and the hashed password
     user_data = {
