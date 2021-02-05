15831ee0d5eb0f6c0e86bbaf4dd8d6d6e952842c
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-02-05 12:13:11 -0800

    cleanup ready for dev

--- server/__init__.py
+++ server/__init__.py
@@ -20,9 +20,13 @@ def create_db(app):
     app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
     app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
     app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+
+    print('Connected to MySQL database')
     
     mysql = MySQL(app)
 
+if __name__ == '__main__':
+    app.run(debug=True)
 
 
 '''
