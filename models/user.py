8bcaac2206a9da18e0399d1e53077db12c66d4ca
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-02-04 20:51:27 -0800

    requirements.txt

--- models/user.py
+++ models/user.py
@@ -1,21 +1,19 @@
-# project/server/models.py
-
 import datetime
-
-from project.server import app, db, bcrypt
-
+import db
 
 class User(db.Model):
     """ User Model for storing user related details """
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(255), unique=False, nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
     registered_on = db.Column(db.DateTime, nullable=False)
     admin = db.Column(db.Boolean, nullable=False, default=False)
 
-    def __init__(self, email, password, admin=False):
+    def __init__(self, name, email, password, admin=False):
+        self.name = name
         self.email = email
         self.password = password
         self.registered_on = datetime.datetime.now()
