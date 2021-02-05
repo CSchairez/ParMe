39873039a405a4a0a09dfe5e03cff05e951cfb3a
Michael Chairez <mchairez@horizon.csueastbay.edu>
2021-02-04 21:43:14 -0800

    Function to initialize db created.

--- server/__init__.py
+++ server/__init__.py
@@ -1,14 +1,28 @@
 from flask import Flask
+from flask_mysqldb import MySQL 
+import MySQLdb.cursors 
+import dotenv
+import sys
+import os
 
 # Blueprints
 from .api.auth.login_routes import auth
 
 def create_app():
     app = Flask(__name__)
-
     app.register_blueprint(auth)
+    create_db(app)
 
     return app
+
+def create_db(app):
+    app.config['MYSQL_HOST'] = os.getenv("MYSQL_HOST")
+    app.config['MYSQL_USER'] = os.getenv("MYSQL_USER")
+    app.config['MYSQL_PASSWORD'] = os.getenv("MYSQL_PASSWORD")
+    app.config['MYSQL_DB'] = os.getenv("MYSQL_DB")
+    
+    mysql = MySQL(app)
+
 
 
 '''
