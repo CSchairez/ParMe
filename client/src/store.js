3513d1bed9beeb84c626b42206baebbb6042a0b6
Tanner Barcelos <tanner.manuel.barcelos@gmail.com>
2021-02-09 15:44:52 -0800

    redux setup. Reference Traversy starter code for auth

---
+++ client/src/store.js
@@ -0,0 +1,38 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+// Reducer imports
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers';
+
+// Root reducer to combine all our reducers
+const rootReducer = combineReducers({
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+});
+
+// See if the users info from a login is present in local storage or not (return null) because a user can be logged in after navigating away and we are storing the logged in user for a good UX
+const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromLocalStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware)),
+);
+
+export default store;
